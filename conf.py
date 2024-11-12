03a56abd63c9ba322d743e8200ba40d1f7913f6d
Olivér Ősz <16599031+oliverosz@users.noreply.github.com>
2024-11-13 00:58:26 +0100

    Extend test.py with new features

--- conf.py
+++ conf.py
@@ -1,7 +1,8 @@
 """Server configuration constants."""
 
-MANDATORY_PORT = 5555
+DEFAULT_PORT = 5555
+IGNORE_SECRETS = True
 ROBOT_REQUEST_TIMEOUT = 0.3  # seconds
-TURN_DURATION = 3  # seconds
+TURN_DURATION = 2  # seconds
 
 DEFAULT_LEVEL_PATH = "test_maps/small_2p.json"
